7fb225d73c30f9276c6085b54901a2afa2c02d16
KU HYUNJIN <kuhyunjin@KUnoMacBook-Pro.local>
2026-05-27 11:46:51 +0900

    feat: Laravel第5章の投稿一覧画面を追加

--- laravel/lesson-app/routes/web.php
+++ laravel/lesson-app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
@@ -9,5 +9,6 @@ Route::get('/', function() {
 });
 
 Route::get('/about', [PageController::class, 'about']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
