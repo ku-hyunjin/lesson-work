8f5f0ea6d788a3d3a363ff1a863a472b5e26756f
KU HYUNJIN <kuhyunjin@KUnoMacBook-Pro.local>
2026-06-05 12:06:36 +0900

    feat: 投稿一覧に検索機能を追加

--- laravel/lesson-app/resources/views/posts/index.blade.php
+++ laravel/lesson-app/resources/views/posts/index.blade.php
@@ -26,6 +26,18 @@
                 <div class="alert success-alert">{{ session('message') }}</div>
                 @endif
 
+                <form class="search-form" action="/posts" method="GET">
+                    <div class="field search-field">
+                        <label class="field-label" for="keyword">キーワード検索</label>
+                        <input class="field-input" id="keyword" type="text" name="keyword" value="{{ $keyword }}" placeholder="タイトルで検索">
+                    </div>
+
+                    <div class="actions">
+                        <button class="primary-button" type="submit">検索する</button>
+                        <a class="button-link secondary-button" href="/posts">検索をリセット</a>
+                    </div>
+                </form>
+
                 <div class="post-list">
                     @forelse ($posts as $post)
                         <article class="post-card stack-sm">
